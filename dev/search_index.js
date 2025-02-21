3ccb6ed9d8b750ce655fde198b91f3b7b392907c
Documenter.jl <documenter@juliadocs.github.io>
2025-02-21 09:15:56 +0000

    build based on 642a8c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TropicalSweepContractor","category":"page"},{"location":"#TropicalSweepContractor","page":"Home","title":"TropicalSweepContractor","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TropicalSweepContractor.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TropicalSweepContractor]","category":"page"}]
+}
